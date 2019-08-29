516d1d401c85f653977e5784324813acffc38125
Clara <Clara@neoterra.co.za>
2019-08-29 11:59:36 +0200

    Initial solution setup

---
+++ MyShop.Core/MyShop.WebUI.Tests1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShop.WebUI.Tests1.Startup))]
+namespace MyShop.WebUI.Tests1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
